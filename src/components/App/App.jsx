7418448656727bdcd057dbc851dd297f09263cc6
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-09-20 22:17:50 +0300

    Add-base-files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,7 @@
+import { Container } from "./App.styled";
+
+export const App = () => {
+    return (
+        <Container>Hello...</Container>
+    )
+};
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+    width: 1280px;
+    padding: 30px 20px;
+    margin: 0 auto;
+`;
